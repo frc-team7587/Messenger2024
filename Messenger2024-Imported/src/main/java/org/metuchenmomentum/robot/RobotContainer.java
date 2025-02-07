8972a7f3ab10b56528219f5ad4fb1e04f0eeb43e
Bilol.B <113650175+Bilol135@users.noreply.github.com>
2025-02-07 17:13:12 -0500

    deleted dublicate swervedrive objects

--- Messenger2024-Imported/src/main/java/org/metuchenmomentum/robot/RobotContainer.java
+++ Messenger2024-Imported/src/main/java/org/metuchenmomentum/robot/RobotContainer.java
@@ -28,7 +28,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class RobotContainer {
     //subsystems
-    private final SwerveDrive drivetrain = new SwerveDrive();
+   // private final SwerveDrive drivetrain = new SwerveDrive();
     private final Intake intake = new Intake(new IntakeSparkMax());
     private final Shooter shooter = new Shooter(new ShooterSparkMax());
     // private final Climber climber = new Climber(new ClimberSparkMax());
@@ -47,7 +47,6 @@ public class RobotContainer {
 
     public RobotContainer() {
         configureBindings();
-        teleopPeriodic();
 
        // NamedCommands.registerCommand("Intake Note", autonomousIntakeNote());
         //NamedCommands.registerCommand("Handoff Note", autonomousHandoffNote());
@@ -55,10 +54,6 @@ public class RobotContainer {
         // NamedCommands.registerCommand("Lower Climbers", autonomousLowerClimber());
     }
 
-
-  public void teleopPeriodic() {
-    drive(true);
-  }
     private void configureBindings() {
         
         // Drive Command
@@ -217,81 +212,7 @@ shooter.stopShooter()
     */
     }  
     
-  // simple proportional turning control with Limelight.
-  // "proportional control" is a control algorithm in which the output is proportional to the error.
-  // in this case, we are going to return an angular velocity that is proportional to the 
-  // "tx" value from the Limelight.
-  double limelight_aim_proportional()
-  {    
-    // kP (constant of proportionality)
-    // this is a hand-tuned number that determines the aggressiveness of our proportional control loop
-    // if it is too high, the robot will oscillate.
-    // if it is too low, the robot will never reach its target
-    // if the robot never turns in the correct direction, kP should be inverted.
-    double kP = .035;
-
-    // tx ranges from (-hfov/2) to (hfov/2) in degrees. If your target is on the rightmost edge of 
-    // your limelight 3 feed, tx should return roughly 31 degrees.
-    double targetingAngularVelocity = LimelightHelpers.getTX("limelight") * kP;
-
-    // convert to radians per second for our drive method
-    targetingAngularVelocity *= DriveConstants.kMaxAngularSpeed;
-
-    //invert since tx is positive when the target is to the right of the crosshair
-    targetingAngularVelocity *= -1.0;
-
-    return targetingAngularVelocity;
-  }
-
-  // simple proportional ranging control with Limelight's "ty" value
-  // this works best if your Limelight's mount height and target mount height are different.
-  // if your limelight and target are mounted at the same or similar heights, use "ta" (area) for target ranging rather than "ty"
-  double limelight_range_proportional()
-  {    
-    double kP = .1;
-    double targetingForwardSpeed = LimelightHelpers.getTY("limelight") * kP;
-    targetingForwardSpeed *= DriveConstants.kMaxSpeed;
-    targetingForwardSpeed *= -1.0;
-    return targetingForwardSpeed;
-  }
-
-  private void drive(boolean fieldRelative) {
-    // Get the x speed. We are inverting this because Xbox controllers return
-    // negative values when we push forward.
-    var xSpeed =
-        -m_xspeedLimiter.calculate(MathUtil.applyDeadband(driverController.getLeftY(), 0.02))
-            * DriveConstants.kMaxSpeed;
-
-    // Get the y speed or sideways/strafe speed. We are inverting this because
-    // we want a positive value when we pull to the left. Xbox controllers
-    // return positive values when you pull to the right by default.
-    var ySpeed =
-        -m_yspeedLimiter.calculate(MathUtil.applyDeadband(driverController.getLeftX(), 0.02))
-            * DriveConstants.kMaxSpeed;
-
-    // Get the rate of angular rotation. We are inverting this because we want a
-    // positive value when we pull to the left (remember, CCW is positive in
-    // mathematics). Xbox controllers return positive values when you pull to
-    // the right by default.
-    var rot =
-        -m_rotLimiter.calculate(MathUtil.applyDeadband(driverController.getRightX(), 0.02))
-            * DriveConstants.kMaxAngularSpeed;
-
-    // while the A-button is pressed, overwrite some of the driving values with the output of our limelight methods
-    if(driverController.getAButton())
-    {
-        final var rot_limelight = limelight_aim_proportional();
-        rot = rot_limelight;
-
-        final var forward_limelight = limelight_range_proportional();
-        xSpeed = forward_limelight;
-
-        //while using Limelight, turn off field-relative driving.
-        fieldRelative = false;
-    }
-
-    drivetrain.drive(xSpeed, ySpeed, rot, fieldRelative, Robot.getPeriod);
-  } 
+  
     
     public Command getAutonomousCommand() {
         return new PathPlannerAuto("Shoot Only");
